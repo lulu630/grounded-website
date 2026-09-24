b7fa4d0776e38ac60f0b0c90a1c37d743430f081
lulu630 <atl012026@gmail.com>
2026-09-24 14:51:18 +0200

    Make card glow follow cursor

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,6 +1,37 @@
 function ServiceCard( {title, description, tags} ) {
+    
+
+    function handleMouseMove(event) {
+        const card = event.currentTarget
+        const rect = card.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+
+        card.style.setProperty("--mouse-x", `${x}px`)
+        card.style.setProperty("--mouse-y", `${y}px`)
+
+        /* 
+        1) currentTarget — card which event handler is attached to.
+        
+        2) rect — card’s size and position in the viewport.
+        
+        3) x and y — mouse position relative to the card’s top-left corner.
+        
+        4) setProperty stores the coordinates in CSS variables for that specific card.
+
+        */
+
+
+
+    }
+
+
     return (
-        <article className="card">
+        <article 
+        className="card"
+        onMouseMove={handleMouseMove}
+        >
+
             <div className="card__title">
                 <h3>{title}</h3>
             </div>
